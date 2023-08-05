27574514947b51ea1350f1922cba39c2238da042
CourtneyS88 <137658251+CourtneyS88@users.noreply.github.com>
2023-08-05 15:04:11 -0500

    first and second task part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Cool Books";
+   let book1 = "https://th.bing.com/th/id/OIP.IjXSQgpeek65QTvREX852QHaLL?w=115&h=183&c=7&r=0&o=5&pid=1.7";
+   let book2 = "https://th.bing.com/th/id/OIP.6QntvTCqAdctdkdOxiy-IQHaKi?w=113&h=180&c=7&r=0&o=5&pid=1.7";
+   let book3 = "https://th.bing.com/th/id/OIP.Ah1xWb-FeGsi4hhAlsFPzQHaLd?w=128&h=174&c=7&r=0&o=5&pid=1.7";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Dune" />
+         <img src={book2} alt="The Diary of a Teenage Girl" />
+         <img src={book3} alt="Giant Days" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,4 @@
+import classes from './ChoresList.module.css';
 export default function ChoresList () {
    return <p>No content is here!</p>;
 }
